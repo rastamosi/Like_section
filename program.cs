fd5a2885794affae44a68e6467d5d4943dac4dfa
Mostafa Ghorbani <48104432+rastamosi@users.noreply.github.com>
2021-09-12 09:47:51 +0300

    like section
    Program to show who and how many people have liked your post!

---
+++ program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+namespace train
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+          var names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Type a name (or hit ENTER to quit): ");
+
+                var input = Console.ReadLine();
+                
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                names.Add(input);
+            }
+
+            if (names.Count > 2)
+                Console.WriteLine("{0}, {1} and {2} others like your post", names[0], names[1], names.Count - 2);
+                
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+                
+            else if (names.Count == 1)
+                Console.WriteLine("{0} likes your post.", names[0]);
+                
+            else
+                Console.WriteLine();
+        }
+
+
+    }
+
+}
+
+
+ 
